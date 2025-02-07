82e76cae1783772d4375a83831c251df27839d4d
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-07 21:17:55 +0200

    lesson2/task 2.8 done

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -140,3 +140,22 @@ switch (true) {
     default:
         console.log('Wrong time!');
 }
+
+// Task 2.8
+// - У змінній day дано якесь число від 1 до 31.
+// Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).\
+let day = 17;
+
+switch (true) {
+    case day >= 1 && day <= 10:
+        console.log('Перша декада');
+        break;
+    case day >= 11 && day <= 20:
+        console.log('Друга декада');
+        break;
+    case day >= 21 && day <= 31:
+        console.log('Треття декада');
+        break;
+    default:
+        console.log('Невірний день!!!');
+}
