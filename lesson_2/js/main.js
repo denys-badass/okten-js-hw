0bcaf4cf689d75f2905dc9d17bba9953b4c3cab3
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-07 21:09:45 +0200

    lesson2/task 2.7 done

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -117,3 +117,26 @@ if (x !== 0) {
 } else {
     console.log('Невірно');
 }
+
+// Task 2.7
+// - Дано змінну time яка рівна числу від 0 до 59.
+// Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+let time = 44;
+
+switch (true) {
+    case time >= 0 && time < 15:
+        console.log('Перша');
+        break;
+    case time >= 15 && time < 30:
+        console.log('Друга');
+        break;
+    case time >= 30 && time < 45:
+        console.log('Треття');
+        break;
+    case time >= 45 && time < 60:
+        console.log('Четверта');
+        break;
+    default:
+        console.log('Wrong time!');
+}
