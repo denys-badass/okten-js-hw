8864c6cc552f7ac9e4e19b70cbb0207a0f04e0fa
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-07 20:30:35 +0200

    lesson2/task 2.4 done

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -61,3 +61,30 @@ let daVinciBook = {
         {name: 'Dan Brown', age: 59},
     ],
 };
+
+// Task 2.4
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password.
+// Вивести в консоль пароль кожного користувача
+let users = [
+    {name: 'Denys Podobrii', username: 'badass', password: '5up3r53cr3t'},
+    {name: 'Alexander Harris', username: 'aharris8x', password: ' HarriX@Lex@21'},
+    {name: 'Evelyn Scott', username: 'evyscot22', password: '$c0ttEvy@99'},
+    {name: 'Daniel Gomez', username: 'dangomez8', password: 'G@mez_D4n!33%'},
+    {name: 'Isabella Lopez', username: 'izzlopezx', password: '!LoP3z_Izz2X'},
+    {name: 'Harper Nelson', username: 'hnelson22', password: 'Nels@HarP99%'},
+    {name: 'Elijah Howard', username: 'elijhow88', password: 'E!lij3Howard@X'},
+    {name: 'Olivia Morris', username: 'liv_morris', password: '$MorrisL1v@33!'},
+    {name: 'Benjamin Cooper', username: 'ben_coopx', password: 'C0op@!B3nj88'},
+    {name: 'Charlotte Diaz', username: 'char_dia8', password: 'CharT3@Dia_22'},
+];
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
