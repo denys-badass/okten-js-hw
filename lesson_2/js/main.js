efaa4edd55d7073038cf1b8a8efb1ff600ef4769
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-07 21:21:40 +0200

    lesson2/task 2.9 done

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -159,3 +159,33 @@ switch (true) {
     default:
         console.log('Невірний день!!!');
 }
+
+// Task 2.9
+// - Скласти розклад на тиждень за домопоги switch.
+// Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день
+// (можна замість плану на день, назву дня англійською).
+let dayOfWeek = prompt("Enter the day of week");
+
+switch (dayOfWeek) {
+    case 1:
+        console.log('Monday');
+        break;
+    case 2:
+        console.log('Tuesday');
+        break;
+    case 3:
+        console.log('Wednesday');
+        break;
+    case 4:
+        console.log('Thursday');
+        break;
+    case 5:
+        console.log('Friday');
+        break;
+    case 6:
+        console.log('Saturday');
+        break;
+    case 7:
+        console.log('Sunday');
+        break;
+}
