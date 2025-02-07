2db531f38a358f32697ba0e73d08a4968f01bf3c
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-07 20:25:03 +0200

    lesson2/task 2.1 done

---
+++ lesson_2/js/main.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// Task 2.1
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+let mixArr = [
+    125,
+    3.14,
+    'hello',
+    true,
+    'Denys',
+    82034,
+    false,
+    ['a', 'b', 'c'],
+    {id: 1, name: 'John'},
+    'Two word string',
+]
+
+console.log(mixArr[0]);
+console.log(mixArr[1]);
+console.log(mixArr[2]);
+console.log(mixArr[3]);
+console.log(mixArr[4]);
+console.log(mixArr[5]);
+console.log(mixArr[6]);
+console.log(mixArr[7]);
+console.log(mixArr[8]);
+console.log(mixArr[9]);
