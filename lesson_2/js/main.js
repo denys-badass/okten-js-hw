fe029af7ccd248375271031f7f6856a9bb3f9994
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-07 21:35:28 +0200

    lesson2/task 2.11 done

--- lesson_2/js/main.js
+++ lesson_2/js/main.js
@@ -207,3 +207,10 @@ if (firstNum < secondNum) {
 } else if (firstNum === secondNum) {
     console.log('Numbers are equal');
 }
+
+// Task 2.11
+// - є змінна х, яка може прийняти будь-яке значення (стрінг, число, undefined, null  і тд включно). Напишіть код який,
+//  буде присвоювати змінній х значення "default"  якщо ви намагаєтесь присвоїти в неї falsy-значення
+//  (хибноподібні, тобто ті, які приводиться до false, а це 0 null undefined і тд).
+let x1;
+x1 = !x1 ? 'default': x1;
