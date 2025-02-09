4797dbb12d32b8e0cb4ed8eac2e69bbd17afb284
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-09 08:30:54 +0200

    lesson3/base/task 3.4 done

--- lesson_3/base/js/main.js
+++ lesson_3/base/js/main.js
@@ -14,5 +14,11 @@ for (let i = 0; i < 10; i++) {
 // Task 3.3
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 for (let i = 0; i < 20; i++) {
-    document.write(`<h1>Header from loop</h1>`);
+    document.write('<h1>Header from loop</h1>');
+}
+
+// Task 3.4
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+for (let i = 0; i < 20; i++) {
+    document.write(`<h1>Header number ${i} from loop</h1>`);
 }
