4e33ae31b0c0944cad84126c84faa3e5fb3e98d8
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-09 08:29:11 +0200

    lesson3/base/task 3.3 done

--- lesson_3/base/js/main.js
+++ lesson_3/base/js/main.js
@@ -10,3 +10,9 @@ for (let i = 0; i < 10; i++) {
 for (let i = 0; i < 10; i++) {
     document.write(`<div>Block number ${i} from loop</div>`);
 }
+
+// Task 3.3
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+for (let i = 0; i < 20; i++) {
+    document.write(`<h1>Header from loop</h1>`);
+}
