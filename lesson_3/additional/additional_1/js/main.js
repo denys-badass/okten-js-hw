2a6ee7734610f2f4221c26660f9e84fc2d65ae77
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-09 18:59:04 +0200

    lesson3/additional1/task3.1.12 done

--- lesson_3/additional/additional_1/js/main.js
+++ lesson_3/additional/additional_1/js/main.js
@@ -228,5 +228,16 @@ for (const element of arrForFill) {
 for (let i = 0; i < 10; i++) {
     const step = `step ${i}`;
     console.log(step);
-    document.write(`<p>${step}</p>`);
+    document.write(`${step} `);
+}
+
+// separate line
+document.write('<hr>');
+
+// Task Additional 3.1.12
+// - Створити цикл for на 100 ітерацій з кроком 1. Вивести поточний номер кроку через console.log та document.write
+for (let i = 0; i < 100; i++) {
+    const step = `step ${i}`;
+    console.log(step);
+    document.write(`${step} `);
 }
