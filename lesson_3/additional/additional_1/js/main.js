d7f4c99a347290ba7123b62d4e1b22d9710a9b0e
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-09 09:18:18 +0200

    lesson3/additional1/task3.1.1 done

---
+++ lesson_3/additional/additional_1/js/main.js
@@ -0,0 +1,17 @@
+// Task Additional 3.1.1
+// --створити масив з:
+// - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+const mixArr = [
+    42, 7, 19, 100, 3,
+    "apple", "banana", "cherry", "grape", "mango",
+    "hello", 25, true, "world", false,
+];
+// I don`t get it. Should I print to console full array or each element separate?
+console.log(mixArr);
+
+for (const element of mixArr) {
+    console.log(element);
+}
