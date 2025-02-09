0f6d650d136ccfb8440df76d8a6d3b0f5c82f939
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-09 08:26:56 +0200

    lesson3/base/task 3.2 done

--- lesson_3/base/js/main.js
+++ lesson_3/base/js/main.js
@@ -1,5 +1,12 @@
 // Task 3.1
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
 for (let i = 0; i < 10; i++) {
-    document.write('<div>Block from loop</div>')
+    document.write('<div>Block from loop</div>');
+}
+
+
+// Task 3.2
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>Block number ${i} from loop</div>`);
 }
