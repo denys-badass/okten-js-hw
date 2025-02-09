06511acd19d965a398f0744af3db9dd4ee134926
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-09 08:56:13 +0200

    lesson3/base/task 3.6 done

--- lesson_3/base/js/main.js
+++ lesson_3/base/js/main.js
@@ -51,3 +51,46 @@ for (const listOfItem of listOfItems) {
     document.write(`<li>${listOfItem}</li>`);
 }
 document.write('</ul>');
+
+// separate line
+document.write('<hr>');
+
+// Task 3.6
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+// ШАБЛОН
+//  <div class="product-card">
+//         <h3 class="product-title">TITLE. Price - PRICE</h3>
+//         <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://us-east-1-shared-usea1-02.graphassets.com/A2lCPH6tTelhrsostvMQpz/auto_image/resize=fit:max,width:640/quality=value:75/U2XuqyczRPSgluTFZ2PA'
+        // Changed link to actually
+    },
+];
+for (const product of products) {
+    document.write(`<div class="product-card">
+        <h3 class="product-title">${product.title}. Price - ${product.price}</h3>
+        <img src="${product.image}" alt="${product.title}" class="product-image">
+    </div>`);
+}
