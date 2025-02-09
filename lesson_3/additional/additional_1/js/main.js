bf9dee46df8efa1e129994c63c41a91529444201
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-09 09:48:16 +0200

    lesson3/additional1/task3.1.3 done

--- lesson_3/additional/additional_1/js/main.js
+++ lesson_3/additional/additional_1/js/main.js
@@ -29,3 +29,112 @@ for (let i = 0; i < 10; i++) {
 for (const element of arrForFill) {
     console.log(element);
 }
+
+// Task Additional 3.1.3
+// - є масив [2,17,13,6,22,31,45,66,100,-18] :
+let numArr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+// 1. перебрати його циклом while
+let i = 0;
+while (i < numArr.length) {
+    console.log(numArr[i]);
+    i++;
+}
+
+// 2. перебрати його циклом for
+for (const number of numArr) {
+    console.log(number);
+}
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+i = 0;
+while (i < numArr.length) {
+    if (i % 2 !== 0) {
+        console.log(numArr[i]);
+    }
+    i++;
+}
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+for (let i = 0; i < numArr.length; i++) {
+    if (i % 2 !== 0) {
+        console.log(numArr[i]);
+    }
+}
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+i = 0;
+while (i < numArr.length) {
+    if (i % 2 === 0) {
+        console.log(numArr[i]);
+    }
+    i++;
+}
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+for (let i = 0; i < numArr.length; i++) {
+    if (i % 2 === 0) {
+        console.log(numArr[i]);
+    }
+}
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+for (let i = 0; i < numArr.length; i++) {
+    if (numArr[i] % 3 === 0) {
+        numArr[i] = 'okten';
+    }
+}
+
+// 8. вивести масив в зворотньому порядку.
+for (let i = numArr.length - 1; i >= 0; i--) {
+    console.log(numArr[i]);
+}
+
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+numArr = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+i = numArr.length - 1;
+while (i >= 0) {
+    console.log(numArr[i]);
+    i--;
+}
+
+for (let i = numArr.length - 1; i >= 0 ; i--) {
+    console.log(numArr[i]);
+}
+
+i = numArr.length - 1;
+while (i >= 0) {
+    if (i % 2 !== 0) {
+        console.log(numArr[i]);
+    }
+    i--;
+}
+
+for (let i = numArr.length - 1; i >= 0; i--) {
+    if (i % 2 !== 0) {
+        console.log(numArr[i]);
+    }
+}
+
+i = numArr.length - 1;
+while (i >= 0) {
+    if (i % 2 === 0) {
+        console.log(numArr[i]);
+    }
+    i--;
+}
+
+for (let i = numArr.length - 1; i >= 0; i--) {
+    if (i % 2 === 0) {
+        console.log(numArr[i]);
+    }
+}
+
+for (let i = numArr.length; i >= 0; i--) {
+    if (numArr[i] % 3 === 0) {
+        numArr[i] = 'okten';
+    }
+}
+
+console.log(numArr);
