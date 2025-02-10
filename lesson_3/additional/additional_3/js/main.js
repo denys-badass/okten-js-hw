aa16231b64685eb472f109892cb7f8e9c81c9856
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-10 10:43:16 +0200

    lesson3/additional3/task3.3.1 done

---
+++ lesson_3/additional/additional_3/js/main.js
@@ -0,0 +1,46 @@
+// Task Additional 3.3.1
+// 1. Створити пустий масив та :
+// a. заповнити його 50 парними числами за допомоги циклу.
+let arrForFill = [];
+let num = 0;
+let i = 0;
+
+while (arrForFill.length < 50) {
+    if (num % 2 === 0) {
+        arrForFill[i] = num;
+        i++;
+    }
+    num++;
+}
+console.log(arrForFill);
+
+// b. заповнити його 50 непарними числами за допомоги циклу.
+arrForFill = [];
+num = 0;
+i = 0;
+
+while (arrForFill.length < 50) {
+    if (num % 2 !== 0) {
+        arrForFill[i] = num;
+        i++;
+    }
+    num++;
+}
+console.log(arrForFill);
+
+// c. Заповнити масив 20ма рандомними числами. (Google: Generate random number JS)
+arrForFill = [];
+
+for (let i = 0; i < 20; i++) {
+    arrForFill[i] = Math.floor(Math.random() * 128);
+}
+console.log(arrForFill);
+
+// d. Заповнити масив 20ма рандомними чисалами в діапазоні від 8 до 732 (Google: Generate random number JS)
+arrForFill = [];
+
+for (let i = 0; i < 20; i++) {
+    arrForFill[i] = Math.floor(Math.random() * 724 + 8);
+}
+
+console.log(arrForFill);
