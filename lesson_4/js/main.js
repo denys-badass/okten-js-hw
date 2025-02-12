c727c6f7405b5dca7d25809b40c1237567751bb1
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-12 08:21:59 +0200

    lesson3/task4.2 done

--- lesson_4/js/main.js
+++ lesson_4/js/main.js
@@ -5,3 +5,9 @@
 function getRectangleArea(a, b) {
     return a * b;
 }
+
+// Task 4.2
+// - створити функцію яка обчислює та повертає площу кола з радіусом r
+function getCircleArea(r) {
+    return Math.PI * r * r;
+}
