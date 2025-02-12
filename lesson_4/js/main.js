92cd49d267eac31086085d55b988a821fa99fdd5
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-12 08:48:41 +0200

    lesson3/task4.12 done

--- lesson_4/js/main.js
+++ lesson_4/js/main.js
@@ -99,3 +99,11 @@ function sum(arr) {
     }
     return sum;
 }
+
+// Task 4.12
+// - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відповідних індексах
+// Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+function swap(arr, index1, index2) {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+    return arr;
+}
