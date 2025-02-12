8e226466ecec3a41395f41f1366cc80e190d119b
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-12 08:56:58 +0200

    lesson4 allign code

--- lesson_4/js/main.js
+++ lesson_4/js/main.js
@@ -111,7 +111,7 @@ function swap(arr, index1, index2) {
 // Task 4.13
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
-function exchange(sumUAH,currencyValues,exchangeCurrency) {
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
     for (const element of currencyValues) {
         if (element.currency === exchangeCurrency) {
             return sumUAH / element.value;
