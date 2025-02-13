1606ea7b9b0359d2a7696c1eb9cf9379a1072c86
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-13 20:14:28 +0200

    lesson5/task5.4 done

--- lesson_5/js/main.js
+++ lesson_5/js/main.js
@@ -7,3 +7,15 @@ const getRectangleArea = (a, b) => a * b;
 // Task 5.2
 // - створити функцію яка обчислює та повертає площу кола з радіусом r
 const getCircleArea = (r) => Math.PI * r * r;
+
+// Task 5.3
+// - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+const getCylinderArea = (h, r) => 2 * Math.PI * r * h;
+
+// Task 5.4
+// - створити функцію яка приймає масив та виводить кожен його елемент
+const printElements = (arr) => {
+    for (const element of arr) {
+        console.log(element);
+    }
+}
