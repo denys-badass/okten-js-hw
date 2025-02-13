43bc79d207c19906804b7e05a6cc39f4f3cce536
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-13 20:20:22 +0200

    lesson5/task5.7 done

--- lesson_5/js/main.js
+++ lesson_5/js/main.js
@@ -33,3 +33,14 @@ const writeUList = (text) => {
     }
     document.write('</ul>');
 }
+
+// Task 5.7
+// - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий.
+// Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+const writeListElements = (text, num) => {
+    document.write('<ul>');
+    for (let i = 0; i < num; i++) {
+        document.write(`<li>${text}</li>`);
+    }
+    document.write('</ul>');
+}
