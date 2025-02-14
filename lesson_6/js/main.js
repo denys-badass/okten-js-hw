d07d750011b49fdb3973181fbc7df4ad338da1bc
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-14 20:55:16 +0200

    lesson6/task6.8 done

--- lesson_6/js/main.js
+++ lesson_6/js/main.js
@@ -53,3 +53,29 @@ const sortNums = (array,direction) => {
         array.sort((a, b) => b - a);
     }
 }
+
+// Task 6.8
+// - є масив
+const coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+//  -- відсортувати його за спаданням за monthDuration
+coursesAndDurationArray.sort((a, b) => b.monthDuration - a.monthDuration);
+
+//  -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+const moreFiveMonthsCourses = coursesAndDurationArray.filter(value => value.monthDuration > 5);
+
+//  -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+const coursesWithId = coursesAndDurationArray.map((value, index) => {
+    return {
+        id: index + 1,
+        title: value.title,
+        monthDuration: value.monthDuration,
+    }
+});
