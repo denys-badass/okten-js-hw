5026cbcf1a55e33fc2e4e6b07a703b4b68079ee2
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-14 20:29:56 +0200

    lesson6/task6.1 done

--- lesson_6/js/main.js
+++ lesson_6/js/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+// Task 6.1
+// - Знайти та вивести довижину настипних стрінгових значень
+//     'hello world', 'lorem ipsum', 'javascript is cool'
+console.log('hello world'.length);
+console.log('lorem ipsum'.length);
+console.log('javascript is cool'.length);
