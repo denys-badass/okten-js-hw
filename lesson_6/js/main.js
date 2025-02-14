335aadb7494b784ddfbe275b95da8b7358586879
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-14 20:42:51 +0200

    lesson6/task6.6 done

--- lesson_6/js/main.js
+++ lesson_6/js/main.js
@@ -34,3 +34,8 @@ const stringToArray = (str) => str.split(' ');
 
 str = 'Ревуть воли як ясла повні';
 let arr = stringToArray(str);
+
+// Task 6.6
+// - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
+let numbers = [10,8,-7,55,987,-1011,0,1050,0];
+numbers = numbers.map(number => number + '');
