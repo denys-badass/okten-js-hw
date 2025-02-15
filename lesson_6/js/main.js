a9123aba1ad6fbd3c6d2b1caf84dc9151dfd07a4
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-15 08:02:27 +0200

    lesson6/task6.11 done

--- lesson_6/js/main.js
+++ lesson_6/js/main.js
@@ -150,3 +150,81 @@ const deckBySuit = cardDeck.reduce((acc, card) => {
     }
     return acc;
 }, {spades: [], diamonds: [], hearts: [], clubs: []});
+
+// Task 6.11
+// взяти з arrays.js масив coursesArray
+const coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+// --написати пошук всіх об'єктів, в яких в modules є sass
+const coursesWithSass = coursesArray.filter(value => value.modules.includes('sass'));
+
+// --написати пошук всіх об'єктів, в яких в modules є docker
+const coursesWithDocker = coursesArray.filter(value => value.modules.includes('docker'));
