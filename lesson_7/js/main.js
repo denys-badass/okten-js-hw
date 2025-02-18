b84b8c9482ecef7344f34391e9d4c0f73d638b18
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-18 21:35:48 +0200

    lesson7/task7.6 done

--- lesson_7/js/main.js
+++ lesson_7/js/main.js
@@ -67,3 +67,41 @@ clients.push(new Client(id.next().value, 'Mia', 'Martinez', 'mia.martinez@exampl
 // - Взяти масив (Client [] з попереднього завдання).
 // Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 clients.sort((a, b) => a.order.length - b.order.length);
+
+// Task 7.6
+// - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель, виробник, рік випуску,
+// максимальна швидкість, об'єм двигуна. додати в об'єкт функції:
+//     -- drive () - яка виводить в консоль `їдемо зі швидкістю ${максимальна швидкість} на годину`
+//     -- info () - яка виводить всю інформацію про автомобіль в форматі `назва поля - значення поля`
+//     -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
+//     -- changeYear (newValue) - змінює рік випуску на значення newValue
+//     -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+function Car(model, manufacturer, releaseYear, maxSpeed, engineVolume) {
+    this.model = model;
+    this.manufacturer = manufacturer;
+    this.releaseYear = releaseYear;
+    this.maxSpeed = maxSpeed;
+    this.engineVolume = engineVolume;
+}
+
+Car.prototype.drive = function () {
+    console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`);
+};
+
+Car.prototype.info = function () {
+    for (const [key, value] of Object.entries(this)) {
+        console.log(`${key} - ${JSON.stringify(value)}`);
+    }
+};
+
+Car.prototype.increaseMaxSpeed = function (newSpeed) {
+    this.maxSpeed += newSpeed;
+};
+
+Car.prototype.changeYear = function (newValue) {
+    this.releaseYear = newValue;
+};
+
+Car.prototype.addDriver = function (driver) {
+    this.driver = driver;
+};
