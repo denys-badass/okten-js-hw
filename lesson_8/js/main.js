9ef615633ea024f8aff358c657dec486559b7164
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-21 06:39:19 +0200

    lesson8/task8.2 done

--- lesson_8/js/main.js
+++ lesson_8/js/main.js
@@ -29,3 +29,18 @@ const deepCopy = obj => {
     
     return copy;
 }
+
+// Task 8.2
+// - є масив
+// за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration
+// Зробити все ВИКЛЮЧНО за допомоги інлайн конструкції
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+const coursesWithId = coursesAndDurationArray.map((value, index) => ({id: index + 1, ...value}));
