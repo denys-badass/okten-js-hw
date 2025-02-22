476cdb6102fcef17cd4a3be9749603da3894fa32
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-22 13:50:40 +0200

    lesson9/task9.1 done

--- lesson_9/base/js/main.js
+++ lesson_9/base/js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+// Task 9.1
+// - створити блок,
+// - додати йому класи wrap, collapse, alpha, beta
+// - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// - додати цей блок в body.
+// - клонувати його повністю, та додати клон в body.
+const body = document.body;
+const firstWrapper = document.createElement('div');
+firstWrapper.classList.add('wrap', 'collapse', 'alpha', 'beta');
+firstWrapper.style.background = 'cyan';
+firstWrapper.style.color = 'gray';
+firstWrapper.style.fontSize = '18px';
+
+body.appendChild(firstWrapper);
+
+const secondWrapper = firstWrapper.cloneNode(true);
+
+body.appendChild(secondWrapper);
