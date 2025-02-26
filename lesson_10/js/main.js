b43b4a87bff34d0932f5a5c337c48e7ab6c4368c
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-26 06:43:14 +0200

    lesson10/task10.2 done

--- lesson_10/js/main.js
+++ lesson_10/js/main.js
@@ -9,3 +9,16 @@ const textForHide = document.getElementById('text');
 hideBtn.onclick = function () {
     textForHide.style.display = 'none';
 }
+
+// Task 10.2
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.
+// При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18,
+// та повідомити про це користувача
+const ageInput = document.getElementById('age-input');
+const ageCheckBtn = document.getElementById('check-age-btn');
+const result = document.getElementById('result');
+
+ageCheckBtn.onclick = function () {
+    const age = +ageInput.value;
+    result.innerText = age > 18 ? 'You age greater than 18' : 'You age less than 18';
+}
