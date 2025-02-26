5fa02bffbee9d29d6b44568431cc4d2f2da9b3e1
Denys Podobrii <denys.podobrii@gmail.com>
2025-02-26 06:18:27 +0200

    lesson10/task10.1 done

--- lesson_10/js/main.js
+++ lesson_10/js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// Task 10.1
+// - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript,
+// зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
+const hideBtn = document.getElementById('hide-btn');
+const textForHide = document.getElementById('text');
+
+hideBtn.onclick = function () {
+    textForHide.style.display = 'none';
+}
