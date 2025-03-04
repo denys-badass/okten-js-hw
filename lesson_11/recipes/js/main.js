d5388f01b24d79f95fc3c22d0cd8eb9819df3045
Denys Podobrii <denys.podobrii@gmail.com>
2025-03-04 14:58:28 +0200

    lesson11/task11.2 set localstorage size of recipes

--- lesson_11/recipes/js/main.js
+++ lesson_11/recipes/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const recipeUrl = new URL('https://dummyjson.com/recipes');
+recipeUrl.searchParams.set('limit', '0');
+
+fetch(recipeUrl)
+    .then(response => response.json())
+    .then((data) => {
+        localStorage.setItem('numOfRecipes', JSON.stringify(data.total));
+    })
+
+console.log(localStorage.getItem('numOfRecipes'));
+
