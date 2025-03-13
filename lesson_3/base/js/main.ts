71222bf6530c0c319360f7e547316b048b4bbfa2
Denys Podobrii <denys.podobrii@gmail.com>
2025-03-13 14:20:29 +0200

    lesson 3 base rewrite to TS

--- lesson_3/base/js/main.ts
+++ lesson_3/base/js/main.ts
@@ -1,17 +1,20 @@
-"use strict";
 // Task 3.1
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
-for (let i = 0; i < 10; i++) {
+for (let i: number = 0; i < 10; i++) {
     document.write('<div>Block from loop</div>');
 }
+
+
 // Task 3.2
 // - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
-for (let i = 0; i < 10; i++) {
+for (let i: number = 0; i < 10; i++) {
     document.write(`<div>Block number ${i} from loop</div>`);
 }
+
 // Task 3.3
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
-let i = 0;
+let i: number = 0;
+
 while (i < 20) {
     document.write('<h1>Header from loop</h1>');
     i++;
@@ -19,10 +22,12 @@ while (i < 20) {
 // Task 3.4
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 i = 0;
+
 while (i < 20) {
     document.write(`<h1>Header number ${i} from loop</h1>`);
     i++;
 }
+
 // Task 3.5
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
 // побудувати структуру по шаблону
@@ -42,15 +47,33 @@ while (i < 20) {
 // </ul>
 //
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
-let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
-document.write('<ul>');
+let listOfItems: string[] = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+
+document.write('<ul>')
 for (const listOfItem of listOfItems) {
     document.write(`<li>${listOfItem}</li>`);
 }
 document.write('</ul>');
+
 // separate line
 document.write('<hr>');
-let products = [
+
+// Task 3.6
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+// ШАБЛОН
+//  <div class="product-card">
+//         <h3 class="product-title">TITLE. Price - PRICE</h3>
+//         <img src="IMAGE" alt="" class="product-image">
+// </div>
+// Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
+type Product = {
+    title: string;
+    price: number;
+    image: string;
+}
+let products: Product[] = [
     {
         title: 'milk',
         price: 22,
@@ -79,21 +102,35 @@ for (const product of products) {
         <img src="${product.image}" alt="${product.title}" class="product-image">
     </div>`);
 }
+
 // Separate line
 document.write('<hr>');
-let users = [
-    { name: 'vasya', age: 31, status: false },
-    { name: 'petya', age: 30, status: true },
-    { name: 'kolya', age: 29, status: true },
-    { name: 'olya', age: 28, status: false },
-    { name: 'max', age: 30, status: true },
-    { name: 'anya', age: 31, status: false },
-    { name: 'oleg', age: 28, status: false },
-    { name: 'andrey', age: 29, status: true },
-    { name: 'masha', age: 30, status: true },
-    { name: 'olya', age: 31, status: false },
-    { name: 'max', age: 31, status: true }
+
+// Task 3.7
+// є масив
+//  за допомоги циклу вивести:
+//  - користувачів зі статусом true
+//  - користувачів зі статусом false
+//  - користувачів які старші за 30 років
+type User = {
+    name: string;
+    age: number;
+    status: boolean;
+}
+let users: User[] = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
 ];
+
 document.write('<h4> True status users:</h4>');
 document.write('<ul>');
 for (const user of users) {
@@ -102,6 +139,7 @@ for (const user of users) {
     }
 }
 document.write('</ul>');
+
 document.write('<h4> False status users:</h4>');
 document.write('<ul>');
 for (const user of users) {
@@ -110,6 +148,7 @@ for (const user of users) {
     }
 }
 document.write('</ul>');
+
 document.write('<h4>Users older than 30:</h4>');
 document.write('<ul>');
 for (const user of users) {
